0dd39e92ed966fd42458dbb51ee282022bebb4cb
m_dasun <m.dasun@directfn.com>
2018-04-11 00:21:19 +0530

    added http requests

--- node_mcu/node_mcu.ino
+++ node_mcu/node_mcu.ino
@@ -1,7 +1,13 @@
-#include "ESP8266WiFi.h"
+#include "ESP8266WiFi.h";
+#include "ESP8266HTTPClient.h";
 
-const char* ssid = "IIT Student WIFI";
-const char* password = "#1234@abcd";
+//const char* ssid = "IIT Student WIFI";
+//const char* password = "#1234@abcd";
+
+const char* ssid = "SLT-LTE-WiFi";
+const char* password = "th8ne88y8qf";
+
+HTTPClient http;
 
 void setup(void) {
   Serial.begin(115200); // Starting serial communication
@@ -28,64 +34,91 @@ void setup(void) {
   Serial.println(WiFi.localIP());
 }
 
+unsigned int integerValue = 0;
+char incomingByte;
+
 void loop() {
-  if (Serial.available () > 0 ) {  // checking any request code from colorlight controller
-    int reqCode = Serial.read();
+  if (Serial.available () > 0 ) {
+      integerValue = 0;
+
+      while(1) {
+        incomingByte = Serial.read();
+        if (incomingByte == '\n') break;
+        if (incomingByte == -1) continue;
+        integerValue *= 10;
     
-    if (reqCode == 100) {
-      get_time_delay ();  // Requesting color light time
-    } else if (reqCode == 200) {
-      update_current_state ();   // Requesting to save current color light state
-    } else if (reqCode == 300) {
-      request_pedestrian_crossing (); // Requesting to pedestrian crossing
-    }
+        integerValue = ((incomingByte - 48) + integerValue);
+      }
+
+      Serial.println(integerValue);
+
+      if (integerValue == 100) {
+        get_time_delay();                     // Requesting color light time
+      } else if (integerValue == 200) {
+        update_current_state();               // Requesting to save current color light state
+      } else if (integerValue == 111) {
+        test();                               // Test function
+      }
+  }
 }
 
 void get_time_delay () {
-  http.post('https://vtraffic.herokuapp.com/getTime/',
-        'Content-Type: application/json\r\n',
-        '{"locationId":"1",
-        "stateId":"5"}', 
-        function(reqStat, data) {
-          if (reqStat < 0) then
-            Serial.println("failed")
-            Serial.write (0);
-          else
-            Serial.println("Success")
-            Serial.write (data.time);
-          end 
-        }       
-  end
+  http.begin("http://18.221.95.10:2000/api/requestTime");
+  http.addHeader("Content-Type", "application/json; charset=utf-8");
+  char *payload = "{\"trafficLightId\":\"\",\"stateId\":\"1\"}";
+  
+  int httpCode = http.POST((uint8_t *)payload,strlen(payload));
+
+  if(httpCode == HTTP_CODE_OK) {
+      Serial.print("HTTP response code ");
+      Serial.println(httpCode);
+      String response = http.getString();
+      Serial.println(response);
+  } else {
+    Serial.println("Error in HTTP request");
+  }
+
+  http.end();
+}
+
+void test () {
+  Serial.println("getDelay");
+  http.begin("http://18.221.95.10:2000/api/test");
+  int httpCode = http.GET();
+
+  if(httpCode == HTTP_CODE_OK) {
+     Serial.print("HTTP response code ");
+     Serial.println(httpCode);
+     String response = http.getString();
+     Serial.println(response);
+  } else {
+     Serial.println("Error in HTTP request");
+  }
+ 
+  http.end();
 }
   
 void update_current_state () {
- http.post('https://vtraffic.herokuapp.com/state/',
-        'Content-Type: application/json\r\n',
-        '{"locationId":"1",
-        "currentState":"2"}', 
-        function(reqStat, data) {
-          if (reqStat < 0) then
-            Serial.println("failed - saveState")
-          else
-            Serial.println("Success - saveState")
-          end 
-        }       
-  end
+ http.begin("http://18.221.95.10:2000/api/updateState");
+  http.addHeader("Content-Type", "application/json; charset=utf-8");
+  char *payload = "{\"trafficLightId\":\"\",\"stateId\":\"1\"}";
+  int httpCode = http.POST((uint8_t *)payload,strlen(payload));
+
+  if(httpCode == HTTP_CODE_OK) {
+      Serial.print("HTTP response code ");
+      Serial.println(httpCode);
+      String response = http.getString();
+      Serial.println(response);
+  } else {
+    Serial.println("Error in HTTP request");
+  }
+
+  http.end();
 }
 
-void request_pedestrian_crossing () {
-  http.post('https://vtraffic.herokuapp.com/pedestrian/',
-          'Content-Type: application/json\r\n',
-          '{"locationId":"1",
-          "crossintId":"1"}', 
-          function(reqStat, data) {
-            if (reqStat < 0) then
-              Serial.println("failed - pedestrian")
-            else
-              Serial.println("Success - pedestrian")
-            end 
-          }       
-    end
+int get_serial_value () {
+  
+  
 }
 
 
